f4ba4d8ba170b2e1a5d71cfd62dcd2db2dbcef42
Cedric Flamain <cedflam@gmail.com>
2020-01-18 20:24:55 +0100

    Implementation of sorting by category in jQuery on the home page

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+// Ce code permet de faire le tri par catégories sur la page d'accueil
+//Je gère l'affichage de tous les tricks
+$('#tous').click(function (event) {
+    event.preventDefault();
+    //Classe courante au clic sur le badge
+    $(this).addClass('current');
+    //J'enlève la class hidden à tous les éléments
+    $('.gallery').find('div.card').removeClass('hidden');
+});
+//Je gère l'affichage des grabs
+$('#grab').click(function (event) {
+    event.preventDefault();
+    //Si le click est détécté alors j'ajoute la classe courante
+    $(this).addClass('current');
+    //J'enlève la classe hidden à l'ensemble des éléments 
+    $('.gallery').find('div.card').removeClass('hidden');
+    //J'ajoute la classe hidden à tous les éléments != grab 
+    $('.gallery').find('div.card:not(.grab)').addClass('hidden');
+});
+//Je gère l'affichage des slides
+$('#slide').click(function (event) {
+    event.preventDefault();
+    $(this).addClass('current');
+    $('.gallery').find('div.card').removeClass('hidden');
+    $('.gallery').find('div.card:not(.slide)').addClass('hidden');
+});
+//Je gère l'affichage des rotation
+$('#rotation').click(function (event) {
+    event.preventDefault();
+    $(this).addClass('current');
+    $('.gallery').find('div.card').removeClass('hidden');
+    $('.gallery').find('div.card:not(.rotation)').addClass('hidden');
+});
+//Je gère l'affichage des flips
+$('#flip').click(function (event) {
+    event.preventDefault();
+    $(this).addClass('current');
+    $('.gallery').find('div.card').removeClass('hidden');
+    $('.gallery').find('div.card:not(.flip)').addClass('hidden');
+});
+//Je gère l'affichage des old school
+$('#old-school').click(function (event) {
+    event.preventDefault();
+    $(this).addClass('current');
+    $('.gallery').find('div.card').removeClass('hidden');
+    $('.gallery').find('div.card:not(.old-school)').addClass('hidden');
+});
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,13 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * Fonction qui permet de récupérer l'ensemble des figures
+     * 
      * @Route("/", name="home")
      */
     public function index(TricksRepository $tricksRepo)
     {
+        //Je stocke l'ensemble des figures dans une variable
         $figures = $tricksRepo->findAll();
         
-
+        //J'affiche la page et je paramètre twig
         return $this->render('home.html.twig',[
             'figures'=> $figures,
             
